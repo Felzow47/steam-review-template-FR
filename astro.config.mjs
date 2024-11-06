46316b33b8b854c85413882a61ea19c142039344
Felzow47 <kantintiti@gmail.com>
2024-11-06 02:15:00 +0100

    Huge Update Color changed Translate all in french add more credit for VojtaStruhar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Felzow47.github.io/",
-  base: "/steam-review-template-FR",
+  base: "/index.astro",
   integrations: [tailwind()],
 });
